e0a268d5fdaa3bcb8ed7df0e06f7b6b8f77636c6
“ismailp97” <irpatel97@outlook.com>
2025-02-05 22:05:32 +0000

    update

--- client/feedbackPage/feedback.js
+++ client/feedbackPage/feedback.js
@@ -2,3 +2,11 @@ document.addEventListener("DOMContentLoaded", () => {
     const userId = localStorage.getItem("email") || "Guest";
     document.querySelector(".user-id").textContent = `Hello, ${userId}`;
 })
+
+function logout() {
+    console.log("Logging out...");
+    localStorage.removeItem("token"); // Clear authentication token
+    localStorage.removeItem("email")
+    localStorage.removeItem("user_id")
+    window.location.href = "../loginPage/login.html"; // Redirect to login page
+}
